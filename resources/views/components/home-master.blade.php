6ae2a0de832fa774be1eb1b93d023800a16054bc
gurjiitsingh <gurjiitsingh@gmail.com>
2020-09-20 15:26:14 +0530

    189 adding link

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -36,6 +36,9 @@
             </a>
           </li>
           <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.index')}}">Admin</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
